2bc86d86eade1a0f4f40b02c47c0d2969b868372
JanWilliamHaug <71844869+JanWilliamHaug@users.noreply.github.com>
2025-05-09 22:03:36 +0200

    temporary removed a menu in the header for now

--- william-and-mamina-forever/src/components/Header.tsx
+++ william-and-mamina-forever/src/components/Header.tsx
@@ -1,6 +1,7 @@
 // src/components/Header.tsx
 import { useLocation, useNavigate } from 'react-router-dom';
-import { ArrowLeft, Menu, User } from 'lucide-react';
+import { ArrowLeft, User } from 'lucide-react';
+//import { ArrowLeft, Menu, User } from 'lucide-react';
 
 const titles: Record<string, string> = {
   '/': 'Home',
@@ -22,7 +23,7 @@ export default function Header() {
      pt-[env(safe-area-inset-top)]
      bg-white bg-opacity-90 backdrop-blur-md
      flex items-center justify-between
-     px-8 h-24                  /* ← ~96 px header */
+     px-8 h-24                  
      shadow-md
    "
  >
@@ -30,7 +31,9 @@ export default function Header() {
         onClick={() => (isHome ? null : navigate(-1))}
         className="p-4"
       >
-        {isHome ? <Menu /> : <ArrowLeft />}
+        {/*{isHome ? <Menu /> : <ArrowLeft />}*/}
+        {isHome ? null : <ArrowLeft />}
+
       </button>
       <h1 className="text-xl font-heading">{titles[pathname] || ''}</h1>
       <User className="w-12 h-12 text-pink-600 cursor-pointer" />
